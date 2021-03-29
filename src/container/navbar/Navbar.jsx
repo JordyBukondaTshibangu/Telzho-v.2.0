1fa12f4eb4ba3b231ce55d2be4357625585f9472
Jordy Tshibangu <jordytshibss@gmail.com>
2021-03-29 16:21:40 +0200

    features : added the user-account-modal and the menu-modal

--- src/container/navbar/Navbar.jsx
+++ src/container/navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({handleUserModal, handleMenuModal}) => {
+
     return (
         <div className="nav-bar">
-            <div className="user-account">
-                <img src="https://cdn0.iconfinder.com/data/icons/thin-users-people/57/thin-191_user_profile_avatar-512.png" alt="/" />
+            <div className="user-account" onClick={() => handleUserModal()}>
+                <img src="https://cdn0.iconfinder.com/data/icons/thin-users-people/57/thin-191_user_profile_avatar-512.png" alt="/" />    
             </div>
-            <div className="menu">
+            <div className="menu" onClick={() => handleMenuModal()}>
                 <img src="https://cdn0.iconfinder.com/data/icons/heroicons-ui/24/icon-menu-512.png" alt="/" />
             </div>
         </div>
--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,16 +1,35 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './Home.css';
 import Navbar from '../../container/navbar/Navbar';
 import Logo from '../../assets/logo.jpeg';
 import Search from '../../components/search/Search';
 import News from '../../container/news/News';
 import ExternalApp from '../../container/externalApp/ExternalApp';
+import UserAccount from '../../modals/userAccount/UserAccount';
+import Menu from '../../modals/menu/Menu';
 
 const Home = () => {
+
+    const [ openUserAccountModal, setOpenUserAccountModal ] = useState(false);
+    const [ openMenuModal, setOpenMenuModal ] = useState(false);
+
+    const handleUserModal = () => { setOpenUserAccountModal(!openUserAccountModal)};
+    const handleMenuModal = () => { setOpenMenuModal(!openMenuModal)};
     
+    console.log(openUserAccountModal);
     return (
         <div className="home">
+            <div>
+                {
+                    openUserAccountModal && ReactDOM.createPortal(<UserAccount />, document.getElementById('user-account-modal'))
+                }
+                {
+                    openMenuModal && ReactDOM.createPortal(<Menu />, document.getElementById('menu-modal'))
+                }
+            </div>
             <div className="home__header">
-                <Navbar />
+                <Navbar handleUserModal={() => handleUserModal()} handleMenuModal={handleMenuModal}/>
             </div>
             <div className="home__body">
                 <img src={Logo} alt="/" />
