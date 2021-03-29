39cabba0b1832832230542ff18f4dd4866672067
Jordy Tshibangu <jordytshibss@gmail.com>
2021-03-29 17:51:00 +0200

    style : change the user logo icon

--- src/modals/menu/Menu.jsx
+++ src/modals/menu/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = ({handleCloseMenuModal}) => {
             <div className = "menu-modal">
                 <div className="menu-actions">
                     <ul>
-                        <li> <a href="/"><img src="https://cdn0.iconfinder.com/data/icons/thin-users-people/57/thin-191_user_profile_avatar-512.png" alt=""/> Login</a></li>
+                        <li> <a href="/"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTezVZRoHiOL2h1sfPm3xB_MY_sKqFAcopRqQ&usqp=CAU" alt=""/> Login</a></li>
                         <li> <a href="/"><img src="https://d29fhpw069ctt2.cloudfront.net/icon/image/39129/preview.png" alt=""/> Phone book</a></li>
                         <li> <a href="/"><img src="https://www.kindpng.com/picc/m/49-496199_icons-envelope-computer-mail-message-email-email-icon.png" alt=""/> Telmail </a></li>
                         <li> <a href="/"><img src="https://static.thenounproject.com/png/331581-200.png" alt=""/> Maps </a></li>
